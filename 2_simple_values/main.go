6aa92b43d398791222a84a423d0f0ae54b7dbec4
Narayan Maity <narayanmaitysp1997@gmail.com>
2024-10-14 16:09:47 +0530

    first commit

---
+++ 2_simple_values/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	//simple values
+
+	//Integers
+	fmt.Println(1)
+
+	//Strings
+	fmt.Println("Hello Worlds!!!😈😈😈")
+
+	//Booleans
+	fmt.Println(true)
+	fmt.Println(false)
+
+	//Floats
+	fmt.Println(10.5)
+
+	//Arithmetic Operations
+	fmt.Println(1463 / 7)
+
+}
