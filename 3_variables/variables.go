8018c83196aa435816c832c009e0c57b2ae32414
Narayan Maity <narayanmaitysp1997@gmail.com>
2024-10-14 17:16:15 +0530

    go earing about variables

---
+++ 3_variables/variables.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "Golang"
+	// var isAdult bool = true
+	// var age int = 30
+	// var price float32 = 50.5
+
+	//infer the type automatically
+	// var name = "Golang"
+	var isAdult = true
+	var age = 30
+	// var price = 50.5
+
+	//Shorhand Syntax
+	name := "Golang"
+	price := 50.5
+
+	//Note: If you are declaring any varaible you must have to use or delete it
+
+	fmt.Println(name)
+	fmt.Println(isAdult)
+	fmt.Println(age)
+	fmt.Println(price)
+}
